2c0c8ac92692457df18b81e4e99a73871a858edf
Hubert Pan <hpan@xmatters.com>
2018-09-23 07:27:50 -0700

    Add confirmation dialog for sending notificaiton

--- backend/src/main/resources/static/referral-detail.js
+++ backend/src/main/resources/static/referral-detail.js
@@ -55,6 +55,17 @@ const ReferralDetail = Vue.component('referral-detail', {
           </div>
         </div>
 
+        <div class="modal fade bd-example-modal-sm" id="notificationSentModal" tabindex="-1" role="dialog" >
+          <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+                <div class="modal-body"><i class="green-check-mark fas fa-check"></i> Message Sent!</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+          </div>
+        </div>
+
         <div class="modal fade" id="notifyModal" tabindex="-1" role="dialog">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -184,6 +195,7 @@ const ReferralDetail = Vue.component('referral-detail', {
                 this.isLoading = false;
                 this.referral = response.data;
                 $("#notifyModal").modal('hide');
+                $("#notificationSentModal").modal('show');
             }));
         }
     }
